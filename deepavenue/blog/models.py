a1515bb20864734d7692d770d4ef716690a3bfe8
ntu2kotrevor <trevordlamini92@gmail.com>
2017-09-06 16:34:24 +0200

    work in progres

--- deepavenue/blog/models.py
+++ deepavenue/blog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailsearch import index
 
 
 class Post(models.Model):
@@ -17,3 +21,28 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
